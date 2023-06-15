5890e03b60b320bc83eb01270f65e58f0fbc0bc2
ALXBBT <isholababatunde27@gmail.com>
2023-06-15 13:00:21 -0700

    C program that prints using putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry point for the program.
+ *
+ * Return:0 if no errors, return non zero if errors
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
